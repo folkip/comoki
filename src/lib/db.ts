ddc27c7aac91136a0fa332eaa1ba59da29e1ba48
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-02 14:54:10 -0500

    :bug: fix package.json

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -6,21 +6,19 @@ import type { Connection } from 'mysql2';
 const { DB_URL } = env;
 
 if (!DB_URL) {
-	throw new Error("Missing ENV vars");
+	throw new Error('Missing ENV vars');
 }
 
 class DB {
-	private conn: Connection | undefined;
+	private conn: Connection | null = null;
 
-	constructor(private url: string) {}
-
-	private async prepare() {
-		this.conn ||= createConnection(this.url);
+	constructor(private url: string) {
+		this.url = url;
 	}
 
 	async getConnection() {
-		this.prepare();
-		return this.conn as Connection;
+		this.conn ||= createConnection(this.url);
+		return this.conn;
 	}
 }
 
