fbf2dca0955fbd91bd051eba17d17bea25e43a07
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-08 15:17:07 -0500

    :bug: Fix buttons colors
    fix #7

--- src/components/Buttons/Button.svelte
+++ src/components/Buttons/Button.svelte
@@ -1,19 +1,17 @@
-<script>
-	export let color, service, svg, starter, finisher;
+<script lang="ts">
+	export let service: string, svg: string, extras: string;
 </script>
 
 <div class="flex flex-wrap mb-3 -mx-3 p-10">
 	<div class="w-full px-3">
-		<button
-			class={`p-3 rounded relative flex items-center w-full px-0 text-white bg-${color}-${starter} btn hover:bg-${color}-${finisher}`}
-		>
+		<button class={`p-3 rounded relative flex items-center w-full px-0 text-white ${extras}`}>
 			<svg
 				class="flex-shrink-0 w-4 h-4 mx-4 text-white opacity-75 fill-current"
 				viewBox="0 0 16 16"
 			>
 				<path d={svg} />
 			</svg>
-			<span class="flex-auto pl-16 pr-8 -ml-16">Continue with {service}</span>
+			<span class="flex-auto pl-16 pr-8 -ml-16">{service}</span>
 		</button>
 	</div>
 </div>
