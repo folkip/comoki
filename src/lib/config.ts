6f9af1cd519d20801e78be1042bb3300195cc9b4
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-09 14:45:58 -0500

    :wrench: Update config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,4 +2,19 @@ import { env } from 'node:process';
 
 import 'dotenv/config';
 
-export const { GH_CLIENT_ID, GH_CLIENT_SECRET, DB_URL } = env;
+const {
+	MODE,
+	GH_DEVELOPMENT_CLIENT_ID,
+	GH_DEVELOPMENT_CLIENT_SECRET,
+	GH_PRODUCTION_CLIENT_ID,
+	GH_PRODUCTION_CLIENT_SECRET
+} = env;
+
+const inDev = MODE === 'development';
+
+const GH_CLIENT_SECRET = inDev ? GH_DEVELOPMENT_CLIENT_SECRET : GH_PRODUCTION_CLIENT_SECRET;
+const GH_CLIENT_ID = inDev ? GH_DEVELOPMENT_CLIENT_ID : GH_PRODUCTION_CLIENT_ID;
+
+export const { DB_URL } = env;
+
+export { GH_CLIENT_SECRET, GH_CLIENT_ID };
