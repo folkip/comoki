24e30cb60a438276aa3ac6bdfd6c9d1d6e6297cb
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-09 16:15:02 -0500

    :sparkles: Log files name

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,3 +1,8 @@
 import pino from 'pino';
 
-export default pino(pino.destination('./logs.txt'));
+const time = new Date();
+
+// Example file -> logs/09-02-2022-15-59-49.log
+const filename = `logs/${time.getUTCDay()}-${time.getUTCMonth()}-${time.getUTCFullYear()}-${time.getUTCHours()}-${time.getUTCMinutes()}-${time.getUTCSeconds()}.log`;
+
+export default pino(pino.destination(filename));
