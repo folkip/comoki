37a1746e170a1a09498bcf720e6d1a94c92443be
Brian3647 <victoris3647@gmail.com>
2022-02-10 15:51:09 +0100

    :bug: Fix #14

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,17 @@
 import pino from 'pino';
+import fs from 'node:fs';
 
 const time = new Date();
 
+if (!fs.existsSync('logs')) {
+	fs.mkdirSync('logs');
+}
+
 // Example file -> logs/09-02-2022-15-59-49.log
 const filename = `logs/${time.getUTCDay()}-${time.getUTCMonth()}-${time.getUTCFullYear()}-${time.getUTCHours()}-${time.getUTCMinutes()}-${time.getUTCSeconds()}.log`;
+
+if (!fs.existsSync(filename)) {
+	fs.writeFileSync(filename, '');
+}
 
 export default pino(pino.destination(filename));
