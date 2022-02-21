92d846102cff346430146b42fb28c91882e5936e
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-02-20 20:54:31 -0500

    :rotating_light: Fix CI

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,12 +2,10 @@ module.exports = {
 	content: ['./src/**/*.svelte'],
 	theme: {
 		extend: {
-			fontFamily:{
-				'Courier': ['Courier Prime', 'Courier', 'monospace'],
+			fontFamily: {
+				Courier: ['Courier Prime', 'Courier', 'monospace']
 			}
 		}
 	},
-	plugins: [
-		require('tailwindcss-textshadow')
-	]
+	plugins: [require('tailwindcss-textshadow')]
 };
